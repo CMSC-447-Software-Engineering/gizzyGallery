3b55851128c5213bb0d27b37fe6fc59becc18a36
r66 <98980598+Rostam-Boroumand-Rad@users.noreply.github.com>
2022-12-05 16:34:32 -0500

    Fixed login screen animation

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,49 @@
-import React, { useRef } from 'react';
+import * as React from 'react';
 import {firestore} from "../firebase"
 import {addDoc, collection} from "firebase/firestore"
 import './home.css';
 
 export default function Home(){
-    const usernameRef = useRef();
-    const passwordRef = useRef();
-    const passwordConfirmRef = useRef();
+    const usernameRef = React.useRef();
+    const passwordRef = React.useRef();
+    const passwordConfirmRef = React.useRef();
     const ref = collection(firestore, "messages");
+    const [signingUp, setSigningUp] = React.useState(true);
+    const [signingIn, setSigningIn] = React.useState(false);
 
+    const upChecked = {
+        transform: 'scale(1)'
+    }
+    const upNotChecked = {
+        transform: 'scale(.6)'
+    }
+    const inChecked = {
+        transform: 'scale(1)'
+    }
+    const inNotChecked = {
+        transform: 'scale(.6)'
+    }
+    const loginMain = {
+        transform: 'translateY(-500px)',
+        transition: '.8s ease-in-out'
+        
+    }
+    const signupMain = {
+        transform: 'translateY(-180px)',
+        transition: '.8s ease-in-out'
+    }
+
+    const handleClickSignUp = () => {
+        console.log("clicked sign up", !signingUp);
+        setSigningUp(!signingUp);
+        setSigningIn(!signingIn);
+    }
+
+    const handleClickSignIn = () => {
+        console.log("clicked sign in ", !signingIn);
+        setSigningIn(!signingIn);
+        setSigningUp(!signingUp);
+    }
 
     const handleSave = async(e) =>{
         e.preventDefault();
@@ -41,19 +76,21 @@ export default function Home(){
     <div className = "container" >
         <div className = "signup">
                 <form onSubmit={handleSave}>
-                    <label htmlFor="chk" aria-hidden="true">Sign up</label>
+                    <label onClick = {handleClickSignUp} className = 'signupLabel' aria-hidden="true" style = {signingUp ? upChecked : upNotChecked}>Sign Up</label>
                     <input name="emailEntry" type="email" placeholder="Email"  ref={usernameRef} ></input>
 					<input name="passwordEntry" type="password" placeholder="Password" ref={passwordRef}></input>
 					<input name="passwordEntry" type="password" placeholder="Confirm Password" ref={passwordConfirmRef}></input>
                     <button>Sign Up</button>
                 </form>
         </div>
-        <div className="login">
+        <div className="login" style = {signingIn ? loginMain : signupMain}>
             <form>
-                <label htmlFor="chk" aria-hidden="true">Login</label>
-		        <button>Login</button>
+                <label onClick = {handleClickSignIn} className='loginLabel' aria-hidden="true" style = {signingIn? inChecked : inNotChecked}>Login</label>
+                <input name="emailEntry" type="email" placeholder="Email"  ></input>
+					<input name="passwordEntry" type="password" placeholder="Password" ></input>
+                <button>Login</button>
             </form>
-	</div>  
+	    </div>  
     </div>
     )
 }
