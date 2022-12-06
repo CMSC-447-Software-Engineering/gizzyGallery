4c9405f12ed687672c86905fbeb23e376fe55954
r66 <98980598+Rostam-Boroumand-Rad@users.noreply.github.com>
2022-12-05 19:03:52 -0500

    Delete pages directory

--- pages/resetPassword.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function resetPassword(){
-  return (
-    <div className = "container" >
-        <h1>Sending resert passwork link to email</h1>
-    </div>
-  )
-}
