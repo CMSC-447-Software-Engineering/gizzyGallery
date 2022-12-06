e917f3dbfcc978131e8ef36b07364ba3c39b6ef9
r66 <98980598+Rostam-Boroumand-Rad@users.noreply.github.com>
2022-12-05 22:54:22 -0500

    Delete src directory

--- src/pages/gizzyGal.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function gizzyGal(){
-  return (
-    <div className = "container" >
-        <h1>This is where users photo album will be</h1>
-    </div>
-  )
-}
