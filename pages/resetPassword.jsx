d41d0d74c8983865981f5c6e5488059232071696
r66 <98980598+Rostam-Boroumand-Rad@users.noreply.github.com>
2022-12-05 19:03:13 -0500

    Add files via upload

---
+++ pages/resetPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function resetPassword(){
+  return (
+    <div className = "container" >
+        <h1>Sending resert passwork link to email</h1>
+    </div>
+  )
+}
