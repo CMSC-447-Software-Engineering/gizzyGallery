6b3f74d964e3313d2add382f6bd1e4143ffe040a
tswang89 <71788129+tswang7@users.noreply.github.com>
2022-12-06 12:52:17 -0500

    Fix the 2 random pictures

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -5,12 +5,13 @@ const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState(null);
   const [url, setUrl] = useState(null);
+  var oldUrl = 0;  
 
   useEffect(() => {
     // references
     const storageRef = projectStorage.ref(file.name);
-    const collectionRef = projectFirestore.collection('images');
-    
+    const collectionRef = projectFirestore.collection('images');    
+
     storageRef.put(file).on('state_changed', (snap) => {
       let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
       setProgress(percentage);
@@ -18,10 +19,14 @@ const useStorage = (file) => {
       setError(err);
     }, async () => {
       const url = await storageRef.getDownloadURL();
-      const createdAt = timestamp();
-      await collectionRef.add({ url, createdAt });
-      setUrl(url);
-      console.log("here");
+      if(oldUrl==0){
+      	const createdAt = timestamp();
+      	await collectionRef.add({ url, createdAt });
+      	setUrl(url);
+        oldUrl=1;
+      }
+      else
+        oldUrl=0;
     });
   }, [file]);
 
