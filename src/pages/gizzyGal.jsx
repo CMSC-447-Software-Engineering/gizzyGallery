f74c0388c3c19f1726892d387bc446c31c8a1ff8
temen-j <r61@umbc.edu>
2022-12-06 18:34:42 -0500

    Some Images
    Added the ability to see some images to the gallery.

--- src/pages/gizzyGal.jsx
+++ src/pages/gizzyGal.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import './gizzyGal.css';
 
+import useFirestore from '../hooks/useFirestore';
+import { motion } from 'framer-motion';
+
 export default function GizzyGal(){
+  const { docs } = useFirestore('images');
   const navigate = useNavigate()
 
   const goToAccount = () => {
@@ -21,7 +25,12 @@ export default function GizzyGal(){
       </div>
       <div className="column middle">
         <h2>Pictures</h2>
-        <p>Pics go here...</p>
+		<div className="img-grid">
+			{docs && docs.map(doc => (
+				<img src={doc.url} width="256" height="256" />
+			))}
+			
+		</div>
       </div>
       <div className = "column right" >
         <h2>Sorting:</h2>
