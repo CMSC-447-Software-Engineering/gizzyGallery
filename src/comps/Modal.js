eb2c46c482a2b3e102a6d8024b49941ca1614734
temen-j <r61@umbc.edu>
2022-12-07 23:51:41 -0500

    Get moving left and right working

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const Modal = ({ setSelectedImg, selectedImg }) => {
+const Modal = ({ setSelectedImg, selectedImg, docs }) => {
 	const handle_keydown = (e) => {
 		if(selectedImg){
-			console.log(selectedImg);
+			let index = -1;
+			for(let i = 0; i < docs.length; i++){
+				if(docs[i].url){
+					if(docs[i].url === selectedImg){
+						index = i;
+					}
+				}
+			}
+			if(index !== -1){
+				let change_amt = 0;
+				if(e.key === "ArrowLeft"){
+					change_amt = docs.length - 1;
+				}
+				else if(e.key === "ArrowRight"){
+					change_amt = 1;
+				}
+				let new_index = (index + change_amt) % docs.length;
+				let new_url = docs[new_index].url;
+				setSelectedImg(new_url);
+				selectedImg = new_url;
+			}
 		}
 	}
 	document.addEventListener('keydown', handle_keydown, true);
--- src/pages/gizzyGal.jsx
+++ src/pages/gizzyGal.jsx
@@ -43,7 +43,7 @@ export default function GizzyGal(){
         </div>
       </div>
         { selectedImg && (
-          <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+          <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} docs={docs}/>
         )}
   </div>
   </>
